2665b6fb8ca55ab71c1420dbac4190a8b0f61c49
Nicola Papale <nico@nicopap.ch>
2021-11-12 10:13:39 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,144 @@
+# Bevy UI Build macros
+
+A collection of macros to speed up the process of defining UIs in bevy.
+
+## Bevy version
+
+We do not specify the bevy version we depend on. The reason is mostly that I
+rely on a personal fork of bevy, and it's a complete hassle to fork every
+bevy library to make them work with my own fork of bevy.
+
+Since this does nothing else than define macros, there is no compiled code in
+this crate. the only requirements for the macro to work is that the few bevy
+symbols we rely on are in scope where the macros are called. The bevy symbols we
+explicity use are:
+
+* `bevy::ecs::system::Insert`
+* `bevy::math::{Rect::{self, all}, Size::new}`
+* `bevy::prelude::Parent`
+* `bevy::ui::{entity::NodeBundle, Style, Val::{Percent, Px}}`
+
+## Macros
+
+The macros are nothing more than wrappers around the struct commonly used when
+defining bevy UIs.
+
+```rust
+// unit! -- UNIT --
+unit!(10 px); unit!(100 pct); 
+// Equivalent to
+Val::Px(10.0); Val::Percent(100.0);
+
+// style! -- STYLE --
+style! {
+  flex_wrap: FlexWrap::Wrap,
+  flex_basis: unit!(90 pct),
+};
+// Equivalent to
+Style {
+   flex_wrap: FlexWrap::Wrap,
+   flex_basis: unit!(90 pct),
+   ..Default::default()
+};
+
+// size! -- SIZE --
+size!(100 pct, 90 pct); size!(90 px, 40 px);
+// Equivalent to
+Size::new(Val::Percent(100.0), Val::Percent(90.0));
+Size::new(Val::Px(90.0), Val::Px(40.0));
+
+// rect! -- RECT --
+rect!(10 px); rect!(5 px, 0 px); rect!(100 pct, 10 pct, 50 pct, 0 pct);
+// Equivalent to
+Rect::all(Val::Px(10.0));
+Rect {
+    left: Val::Px(5.0),
+    right: Val::Px(5.0),
+    top: Val::Px(0.0),
+    bottom: Val::Px(0.0),
+};
+Rect {
+    left: Val::Percent(100.0),
+    top: Val::Percent(10.0),
+    right: Val::Percent(50.0),
+    bottom: Val::Percent(0.0),
+};
+
+// build_ui! -- BUILD_UI --
+build_ui! {
+    #[cmd(commands)]
+    vertical{size:size!(100 pct, 100 pct)}(
+        horizontal{justify_content: FlexStart, flex_basis: unit!(10 pct)}(
+            tab_square[focus], tab_square[focus], tab_square[focus]
+        ),
+        column_box(
+            column[red](
+                vertical(select_square, select_square),
+                horizontal{flex_wrap: Wrap}[gray](
+                    square[focus], square[focus], square[focus], square[focus],
+                    square[focus], square[focus], square[focus], square[focus],
+                    square[focus], square[focus], square[focus], square[focus]
+                ),
+                horizontal{flex_wrap: Wrap}[gray](
+                    square[focus], square[focus], square[focus], square[focus],
+                    square[focus], square[focus], square[focus], square[focus]
+                )
+            )
+        )
+    )
+}
+// Equivalent to
+commands.spawn_bundle(NodeBundle {
+    style: Style { size: size!(100 pct, 100 pct), .. vertical.style },
+    .. vertical
+})
+  .with_children(|cmds| {
+    cmds.spawn_bundle(NodeBundle {
+        style: Style {justify_content: FlexStart, flex_basis: unit!(10 pct), .. horizontal.style },
+        .. horizontal
+    })
+      .with_children(|cmds| {
+        cmds.spawn_bundle(tab_square).insert(focus);
+        cmds.spawn_bundle(tab_square).insert(focus);
+        cmds.spawn_bundle(tab_square).insert(focus);
+      });
+    cmds.spawn_bundle(column_box)
+      .with_children(|cmds| {
+        cmds.spawn_bundle(column).insert(red)
+          .with_children(|cmds| {
+            vertical.with_children(|cmds| {
+              cmds.spawn_bundle(select_square);
+              cmds.spawn_bundle(select_square);
+            });
+            cmds.spawn_bundle(NodeBundle {
+                style: Style {flex_wrap: Wrap, ..horizontal.style},
+                .. horizontal
+            }).insert(gray)
+              .with_children(|cmds| {
+                for _ in 0..12 {
+                  cmds.spawn_bundle(square).insert(focus);
+                }
+              });
+            cmds.spawn_bundle(NodeBundle {
+                style: Style {flex_wrap: Wrap, ..horizontal.style},
+                .. horizontal
+            }).insert(gray)
+              .with_children(|cmds| {
+                for _ in 0..8 {
+                  cmds.spawn_bundle(square).insert(focus);
+                }
+              });
+          });
+      });
+  });
+```
+
+## License
+
+This is weird. Since this code literally cannot produce distributable binary
+code, I don't think any of the mainstream license apply. On top of that, I
+usually license libraries I make with less than 100 lines of code under the
+WTFPL. But I'll be a good community player and dual license under MIT or Apache
+2.0 at your leisure.
+
+Copyright © Nicola Papale, see LICENSE file for licensing details.
---
+++ src/lib.rs
@@ -0,0 +1,229 @@
+/// Wrapper around the `bevy::ui::Val` enum
+///
+/// # Syntax
+/// * `unit!(num1 px)` ⇒ `Val::Px(num1 as f32)`
+/// * `unit!(num1 pct)` ⇒ `Val::Percent(num1 as f32)`
+#[macro_export]
+macro_rules! unit {
+    (@with_value px $value:literal) => ( bevy::ui::Val::Px($value as f32));
+    (@with_value pct $value:literal) => ( bevy::ui::Val::Percent($value as f32));
+    ($value:literal $val_unit:ident) => ( unit!(@with_value $val_unit $value));
+}
+
+#[macro_export]
+macro_rules! style {
+    (@default ($default:expr) $($field:ident : $content:expr),*) => (
+        bevy::ui::Style { $($field : $content,)* .. $default }
+    );
+    ($($field:ident : $content:expr,)*) => (
+        style!(@default (Default::default()) $($field : $content),*)
+    );
+}
+
+/// Wrapper around `bevy::math::Size::new`
+///
+/// # Syntax
+/// * `size!(num1 val1, num2 val2)` ⇒ `Size::new(unit!(num1 val1), unit!(num2 val2))`
+#[macro_export]
+macro_rules! size {
+    ($x:literal $x_unit:ident, $y:literal $y_unit:ident) => (
+        bevy::math::Size::new(unit!($x $x_unit), unit!($y $y_unit))
+    );
+}
+
+/// Define a `bevy::math::Rect` similarly to how you would define it in CSS.
+///
+/// # Syntax
+/// ```rust,ignore
+/// // one argument
+/// rect!(num1 val1) == Rect::all(unit!(num1 val1))
+///
+/// // two arguments
+/// rect!(num1 val1, num2 val2) == Rect {
+///     left: unit!(num1 val1),
+///     right: unit!(num1 val1),
+///     top: unit!(num2 val2),
+///     bottom: unit!(num2 val2),
+/// }
+///
+/// // four arguments
+/// rect!(num1 val1, num2 val2, num3 val3, num4 val4) == Rect {
+///     left: unit!(num1 val1),
+///     top: unit!(num2 val2),
+///     right: unit!(num3 val3),
+///     bottom: unit!(num4 val4),
+/// }
+/// ```
+#[macro_export]
+macro_rules! rect {
+    ($x:literal $x_unit:ident) => (
+        bevy::math::Rect::all(unit!($x $x_unit))
+    );
+    (
+        $left:literal $left_unit:ident, $top:literal $top_unit:ident,
+        $right:literal $right_unit:ident, $bottom:literal $bottom_unit:ident,
+    ) => (
+        bevy::math::Rect {
+            left: unit!($left $left_unit),
+            top: unit!($top $top_unit),
+            right: unit!($right $right_unit),
+            bottom: unit!($bottom $bottom_unit),
+        }
+    );
+    ($x:literal $x_unit:ident, $y:literal $y_unit:ident) => (
+        bevy::math::Rect {
+            left: unit!($x $x_unit),
+            top: unit!($y $y_unit),
+            right: unit!($x $x_unit),
+            bottom: unit!($y $y_unit),
+        }
+    );
+}
+
+/// Define a bevy UI and spawns it using `cmd`
+///
+/// # Syntax
+/// ```rust,ignore 
+/// use bevy::prelude::*;
+/// let commands: Commands;
+/// let my_id: Entity;
+/// build_ui! {
+///     // The bevy `Commands`
+///     #[cmd(commands)]
+///     // The "preset" is an identifier, see doc
+///     $entity
+///         // Style modifiers. Supposing $entity is a `NodeBundle`, does:
+///         // $entity.style = style!{ flex_whatever: Whatever }
+///         // Leads to a compilation error if $entity doesn't have a `style`
+///         // field
+///         { flex_whatever: Whatever }
+///         // Additional component. Translates to $entity.insert(comp1).insert(comp2)
+///         [comp1, comp2] 
+///         // Children entities, may have {..}, [..] and (..) 
+///         (
+///             entity[ButtonBundle](square),
+///             id(my_id)
+///         )
+/// }
+/// ```
+///
+/// The `$entity` in the macro may be one of the following:
+/// * `id(Entity)`: inserts a pre-existing entity as child of containing entity
+/// * `$ident`: where `$ident` is the name of a local variable of type 
+///   `T: ComponentBundle`. Spawn the bundle as base to insert extra components
+///   to. Useful to not repeat yourself.
+/// * `entity`: spawn an empty bundle as base to insert extra components to.
+///
+/// # Example
+///
+/// ```rust,ignore
+/// build_ui! {
+///     #[cmd(commands)]
+///     vertical{size:size!(100 pct, 100 pct)}(
+///         horizontal{justify_content: FlexStart, flex_basis: unit!(10 pct)}(
+///             tab_square[focus], tab_square[focus], tab_square[focus]
+///         ),
+///         column_box(
+///             column[red](
+///                 vertical(select_square, select_square),
+///                 horizontal{flex_wrap: Wrap}[gray](
+///                     square[focus], square[focus], square[focus], square[focus],
+///                     square[focus], square[focus], square[focus], square[focus],
+///                     square[focus], square[focus], square[focus], square[focus]
+///                 ),
+///                 horizontal{flex_wrap: Wrap}[gray](
+///                     square[focus], square[focus], square[focus], square[focus],
+///                     square[focus], square[focus], square[focus], square[focus]
+///                 )
+///             )
+///         )
+///     )
+/// }
+/// // Basically becomes
+/// commands.spawn_bundle(NodeBundle {
+///     style: Style { size: size!(100 pct, 100 pct), .. vertical.style },
+///     .. vertical
+/// })
+///   .with_children(|cmds| {
+///     cmds.spawn_bundle(NodeBundle {
+///         style: Style {justify_content: FlexStart, flex_basis: unit!(10 pct), .. horizontal.style },
+///         .. horizontal
+///     })
+///       .with_children(|cmds| {
+///         cmds.spawn_bundle(tab_square).insert(focus);
+///         cmds.spawn_bundle(tab_square).insert(focus);
+///         cmds.spawn_bundle(tab_square).insert(focus);
+///       });
+///     cmds.spawn_bundle(column_box)
+///       .with_children(|cmds| {
+///         cmds.spawn_bundle(column).insert(red)
+///           .with_children(|cmds| {
+///             vertical.with_children(|cmds| {
+///               cmds.spawn_bundle(select_square);
+///               cmds.spawn_bundle(select_square);
+///             });
+///             cmds.spawn_bundle(NodeBundle {
+///                 style: Style {flex_wrap: Wrap, ..horizontal.style},
+///                 .. horizontal
+///             }).insert(gray)
+///               .with_children(|cmds| {
+///                 for _ in 0..12 {
+///                   cmds.spawn_bundle(square).insert(focus);
+///                 }
+///               });
+///             cmds.spawn_bundle(NodeBundle {
+///                 style: Style {flex_wrap: Wrap, ..horizontal.style},
+///                 .. horizontal
+///             }).insert(gray)
+///               .with_children(|cmds| {
+///                 for _ in 0..8 {
+///                   cmds.spawn_bundle(square).insert(focus);
+///                 }
+///               });
+///           });
+///       });
+///   });
+/// ```
+#[macro_export]
+macro_rules! build_ui {
+    (@preset entity) => (());
+    (@preset $anything_else:ident) => ($anything_else);
+    (@preset $node:ident {$($styles:tt)*}) => (
+        bevy::ui::entity::NodeBundle {
+            style: style!(@default ($node.style.clone()) $($styles)*),
+            .. $node.clone()
+        }
+    );
+    (#[cmd($cmds:expr)] id ( $id:expr )) => ({
+        use bevy::ecs::system::Insert;
+        let parent = $cmds.parent_entity();
+        let insert = bevy::ecs::system::Insert {
+            entity: $id,
+            component: bevy::prelude::Parent(parent),
+        };
+        $cmds.add_command(insert);
+    });
+    (#[cmd($cmds:expr)] $preset:ident
+        $( {$($styles:tt)*} )? // {..} style modifiers
+        $( [$($components:expr),*] )? // [..] components
+        $( ( $( // (..) children
+            $preset_chd:ident
+                $( {$($styles_chd:tt)*} )? // {..} children style modifiers
+                $( [$($components_chd:expr),*] )? // [..] children components
+                $( ( $($chd_chd:tt)* ) )? // Children children
+        ),* ) )?
+    ) => (
+        #[allow(unused_variables)]
+        $cmds.spawn_bundle(build_ui!(@preset $preset $({$($styles)*})?).clone())
+            $($(.insert($components.clone()))*)?
+            $(.with_children(|cmds| {
+                $( build_ui!(
+                    #[cmd(cmds)]
+                    $preset_chd
+                        $( { $($styles_chd)* } )?
+                        $( [ $($components_chd),* ] )?
+                        $( ( $($chd_chd)* ) )?
+                ); )*
+            }))?
+    );
+}
